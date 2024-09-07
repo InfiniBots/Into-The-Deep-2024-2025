13ba977700c1a07d541e2366c87ad34d656d2219
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2024-09-07 10:36:21 -0700

    Deletosad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldDrivetrain.java
@@ -6,8 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import java.lang.Math;
-
 
 @TeleOp
 public class OldDrivetrain extends OpMode {
@@ -22,39 +20,14 @@ public class OldDrivetrain extends OpMode {
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontRightMotor.setPower(4);
+        frontLeftMotor.setPower(1);
+        sleep(100);
+        frontLeftMotor.setPower(0);
     }
 
     @Override
     public void loop() {
 
-        double x = gamepad1.left_stick_x;
-        double y = -gamepad1.left_stick_y;
-        double turn = gamepad1.right_stick_x;
-
-        double theta = Math.atan2(y, x);
-        double power = Math.hypot(x, y);
-
-        double sin = Math.sin(theta - Math.PI / 4);
-        double cos = Math.cos(theta - Math.PI / 4);
-        double max = Math.max(Math.abs(sin), Math.abs(cos));
-
-        double frontLeft = power * cos / max + turn;
-        double frontRight = power * sin / max - turn;
-        double backLeft = power * sin / max + turn;
-        double backRight = power * cos / max - turn;
-
-        if ((power + Math.abs(turn)) > 1) {
-            frontLeft /= power + Math.abs(turn);
-            frontRight /= power + Math.abs(turn);
-            backLeft /= power + Math.abs(turn);
-            backRight /= power + Math.abs(turn);
-        }
-
-        frontLeftMotor.setPower(frontLeft);
-        backLeftMotor.setPower(backLeft);
-        frontRightMotor.setPower(frontRight);
-        backRightMotor.setPower(backRight);
-
     }
 }
-
