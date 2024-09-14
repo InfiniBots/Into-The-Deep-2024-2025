d0c4228a8fdd32ffdad681f83a6a996eed9c87a1
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2024-09-14 14:44:39 -0700

    Vatsal Commit and Push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gusamder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gusamder.java
@@ -11,30 +11,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp
 public class gusamder extends OpMode {
-    DcMotor frontLeftMotor;
-    DcMotor frontRightMotor;
-    DcMotor backLeftMotor;
-    DcMotor backRightMotor;
-
-    Servo left;
 
     @Override
     public void init() {
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        left = hardwareMap.get(Servo.class, "left");
-
-
-
-
 
     }
 
     @Override
     public void loop() {
-        frontRightMotor.setPower(gamepad1.right_stick_x);
 
     }
 }
+
