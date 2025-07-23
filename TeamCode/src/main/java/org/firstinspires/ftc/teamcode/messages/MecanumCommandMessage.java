aa103703dd3dd98660535314de6e9a8912e029e0
Vatsal1118 <63930032+Vatsal1118@users.noreply.github.com>
2025-07-23 16:47:38 -0700

    seeyuh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/messages/MecanumCommandMessage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/messages/MecanumCommandMessage.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Roadrunner.messages;
+package org.firstinspires.ftc.teamcode.messages;
 
 public final class MecanumCommandMessage {
     public long timestamp;
